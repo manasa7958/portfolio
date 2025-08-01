0aed813ca2ad6ad12a7b6e0f541fd0341e382f9e
manasa7958 <manasapraveen.work@gmail.com>
2025-08-01 16:59:07 -0400

    Create Footer.tsx

---
+++ components/sections/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-200 text-gray-700 text-center py-4">
+      © {new Date().getFullYear()} Manasa Praveen – Built with ❤️ and too much coffee.
+    </footer>
+  )
+}
